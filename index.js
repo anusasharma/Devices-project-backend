258a6594179083bea7350a0a7f4a76c5c5a8ab7d
Anusa <anusoruban@gmail.com>
2022-03-05 20:59:32 +0530

    Changes

--- index.js
+++ index.js
@@ -28,10 +28,6 @@ const port = 3000;
 const app = express();
 app.use(cors())
 
-app.get('/', async (req, res, next) => {
-    res.send('Hello')
-})
-
 var MongoClient = require('mongodb').MongoClient;
 
 
